9a5f69e553e997f6704477e0686fd16217f90333
Yuri Cianci <yucianci@gmail.com>
2022-08-05 17:55:39 -0300

    added form in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Card, Wrapper } from "./styles";
+import React from "react"
+import { Button, Card, Form, Templates, Wrapper } from "./styles"
 import logo from '../../assets/logo.svg'
 
 export const Home = () => {
@@ -8,7 +8,23 @@ export const Home = () => {
       <img src={logo} alt="logo meme maker" />
 
       <Card>
-        <h2>Selecione um template</h2>
+        <h2>Selecione um template:</h2>
+        <Templates>
+          <button type="button">
+            <img src="" alt="Template 1" />
+          </button>
+          <button type="button">
+            <img src="" alt="Template 1" />
+          </button>
+        </Templates>
+
+        <h2>Textos:</h2>
+        <Form>
+          <input placeholder="Texto 1" />
+          <input placeholder="Texto 2" />
+
+          <Button type="submit">MakeMyMeme!</Button>
+        </Form>
       </Card>
     </Wrapper>
   )
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -19,6 +19,57 @@ export const Card = styled.div`
 
   h2 {
     font-size: 22px;
-    color: #392d2d
+    color: #392d2d;
+    margin-bottom: 10px;
+  }
+`
+
+export const Templates = styled.div`
+  width: 100%;
+  height: 90px;
+  background: #eee;
+  border-radius: 8px;
+  overflow-y: auto;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+  margin-bottom: 30px;
+
+  button {
+    border: 0;
+    background: transparent;
+    margin-right: 10px;
+    img {
+      width: 53px;
+      height: 53px;
+    }
+  }
+`
+
+export const Form = styled.form`
+  input {
+    width: 100%;
+    height: 40px;
+    border-radius: 8px;
+    border: 1px solid #dbdbdb;
+    padding: 0 15px;
+    font-size: 14px;
+    margin-bottom: 10px;
+  }
+`
+
+export const Button = styled.button`
+  width: 100%;
+  height: 40px;
+  border-radius: 8px;
+  border: 0;
+  background: #4395d8;
+  color: #fff;
+  font-weight: bold;
+  transition: background 0.2s ease-in;
+  font-size: 14px;
+
+  :hover {
+    background: #3672a3;
   }
 `
